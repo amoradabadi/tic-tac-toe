f1d245951ec93dd27984ca0ac9e90c05ace23af8
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-09 13:15:16 +0200

    Add dummy test and JUnit dependency

---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TicTacToeTest {
+    @Test
+    void dummy() {
+        assertTrue(true);
+    }
+}
