5b483e4f0c1c19657e3c2cc32bad39cd79a69621
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-12 13:51:25 +0200

    get markers from input, init players

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,16 +1,25 @@
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class TicTacToe {
     public static final String NEW_LINE = "\n";
+    private static final String PIPE = "|";
     public final char EMPTY = ' ';
-
     public char[][] board = new char[][]{
             new char[]{EMPTY, EMPTY, EMPTY},
             new char[]{EMPTY, EMPTY, EMPTY},
             new char[]{EMPTY, EMPTY, EMPTY}
     };
-
+    public Player[] player = new Player[2];
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+    }
+
+    public Optional<Marker> getMarkerFromUserInput(Scanner scanner) {
+        String next = scanner.next();
+        return Marker.getValue(next.trim());
     }
 
     public String showInstructions() {
@@ -21,21 +30,59 @@ public class TicTacToe {
                 Example: 1x3 puts the sign in the first row and third column.
                                 
                 To begin, choose who starts the game by entering X or O.
+                To exit the game press ^C
                 """;
     }
 
     public String printTable() {
         final String separatorRow = "+---+---+---+";
-        final String pipe = "|";
+        final String cellFormat = "%-3s";
         StringBuilder sb = new StringBuilder();
         for (char[] chars : this.board) {
             sb.append(separatorRow).append(NEW_LINE);
-            for (char aChar : chars) {
-                sb.append(pipe).append("%-3s".formatted(aChar));
-            }
-            sb.append(pipe).append(NEW_LINE);
+            for (char aChar : chars) sb.append(PIPE).append(cellFormat.formatted(aChar));
+            sb.append(PIPE).append(NEW_LINE);
         }
         sb.append(separatorRow);
         return sb.toString();
+    }
+
+    public void startGame(Scanner scanner) {
+        showInstructions();
+        Marker marker = getFirstPlayerMarker(scanner);
+        initializePlayers(marker);
+    }
+
+    private void initializePlayers(Marker firstPlayer) {
+        this.player[0] = new Player(firstPlayer);
+        this.player[1] = new Player(firstPlayer.next());
+    }
+
+    private Marker getFirstPlayerMarker(Scanner scanner) {
+        Optional<Marker> optionalMarker;
+        do {
+            optionalMarker = getMarkerFromUserInput(scanner);
+            if (optionalMarker.isEmpty()) {
+                System.out.println("Invalid value, To begin, choose who starts the game by entering X or O.");
+            }
+        } while (optionalMarker.isEmpty());
+        return optionalMarker.get();
+    }
+
+    enum Marker {
+        X, O;
+
+        public static Optional<Marker> getValue(String value) {
+            return Arrays.stream(Marker.values())
+                    .filter(marker -> marker.name().equalsIgnoreCase(value))
+                    .findFirst();
+        }
+
+        public Marker next() {
+            return this == O ? X : O;
+        }
+    }
+
+    record Player(Marker marker) {
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,7 +1,13 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Optional;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TicTacToeTest {
     TicTacToe ticTacToe;
@@ -18,7 +24,8 @@ class TicTacToeTest {
                         "To select a cell for your move enter the position of the row and the column, separated with an x.\n" +
                         "Example: 1x3 puts the sign in the first row and third column.\n" +
                         "\n" +
-                        "To begin, choose who starts the game by entering X or O.\n",
+                        "To begin, choose who starts the game by entering X or O.\n" +
+                        "To exit the game press ^C\n",
                 this.ticTacToe.showInstructions());
     }
 
@@ -46,6 +53,71 @@ class TicTacToeTest {
                         +---+---+---+
                         |   |   |   |
                         +---+---+---+""", this.ticTacToe.printTable());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"x", "X", " X", "X ", " X "})
+    void whenChoosingMarker_shouldReturnX(String userInput) {
+        Scanner scanner = new Scanner(userInput);
+        Optional<TicTacToe.Marker> markerOptional = this.ticTacToe.getMarkerFromUserInput(scanner);
+
+        assertTrue(markerOptional.isPresent());
+        assertEquals(TicTacToe.Marker.X, markerOptional.get());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"o", "O", " O", "O ", " O "})
+    void whenChoosingMarker_shouldReturnO(String userInput) {
+        Scanner scanner = new Scanner(userInput);
+        Optional<TicTacToe.Marker> markerOptional = this.ticTacToe.getMarkerFromUserInput(scanner);
+
+        assertTrue(markerOptional.isPresent());
+        assertEquals(TicTacToe.Marker.O, markerOptional.get());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Something else", "XX", "XO"})
+    void whenChoosingMarkerIsInvalid_shouldReturnNull(String userInput) {
+        Scanner scanner = new Scanner(userInput);
+        Optional<TicTacToe.Marker> markerOptional = this.ticTacToe.getMarkerFromUserInput(scanner);
+
+        assertTrue(markerOptional.isEmpty());
+    }
+
+    @Test
+    void whenStartGame_shouldSetMarkerForX() {
+        String userInput = "X";
+        Scanner scanner = new Scanner(userInput);
+
+        this.ticTacToe.startGame(scanner);
+
+        assertEquals(this.ticTacToe.player[0].marker(), TicTacToe.Marker.X);
+        assertEquals(this.ticTacToe.player[1].marker(), TicTacToe.Marker.O);
+    }
+
+    @Test
+    void whenStartGame_shouldSetMarkerForO() {
+        String userInput = "O";
+        Scanner scanner = new Scanner(userInput);
+
+        this.ticTacToe.startGame(scanner);
+
+        assertEquals(this.ticTacToe.player[0].marker(), TicTacToe.Marker.O);
+        assertEquals(this.ticTacToe.player[1].marker(), TicTacToe.Marker.X);
+    }
+
+    @Test
+    void whenStartGame_shouldKeepAsking() {
+        String userInput = "some\n" +
+                "invalid\n" +
+                "texts\n" +
+                "X";
+        Scanner scanner = new Scanner(userInput);
+
+        this.ticTacToe.startGame(scanner);
+
+        assertEquals(this.ticTacToe.player[0].marker(), TicTacToe.Marker.X);
+        assertEquals(this.ticTacToe.player[1].marker(), TicTacToe.Marker.O);
     }
 
 }
