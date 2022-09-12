0d2e2bac9ef226260c1fdab4eff5867d6789280e
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-12 11:55:48 +0200

    add board  and basic print instructions and print table

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,5 +1,41 @@
 public class TicTacToe {
+    public static final String NEW_LINE = "\n";
+    public final char EMPTY = ' ';
+
+    public char[][] board = new char[][]{
+            new char[]{EMPTY, EMPTY, EMPTY},
+            new char[]{EMPTY, EMPTY, EMPTY},
+            new char[]{EMPTY, EMPTY, EMPTY}
+    };
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
+    }
+
+    public String showInstructions() {
+        return """
+                Welcome to Tic-Tac-Toe
+                                
+                To select a cell for your move enter the position of the row and the column, separated with an x.
+                Example: 1x3 puts the sign in the first row and third column.
+                                
+                To begin, choose who starts the game by entering X or O.
+                """;
+    }
+
+    public String printTable() {
+        final String separatorRow = "+---+---+---+";
+        final String pipe = "|";
+        StringBuilder sb = new StringBuilder();
+        for (char[] chars : this.board) {
+            sb.append(separatorRow).append(NEW_LINE);
+            for (char aChar : chars) {
+                sb.append(pipe).append("%-3s".formatted(aChar));
+            }
+            sb.append(pipe).append(NEW_LINE);
+        }
+        sb.append(separatorRow);
+        return sb.toString();
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,10 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TicTacToeTest {
-    @Test
+    TicTacToe ticTacToe;
+
+    @BeforeEach
     void dummy() {
-        assertTrue(true);
+        this.ticTacToe = new TicTacToe();
     }
+
+    @Test
+    void shouldShowInstructions() {
+        assertEquals("Welcome to Tic-Tac-Toe\n" +
+                        "\n" +
+                        "To select a cell for your move enter the position of the row and the column, separated with an x.\n" +
+                        "Example: 1x3 puts the sign in the first row and third column.\n" +
+                        "\n" +
+                        "To begin, choose who starts the game by entering X or O.\n",
+                this.ticTacToe.showInstructions());
+    }
+
+    @Test
+    void shouldHaveTheCorrectBoardLength() {
+        assertEquals(3, this.ticTacToe.board.length);
+        assertEquals(3, this.ticTacToe.board[0].length);
+    }
+
+    @Test
+    void shouldInitializeBoardWithEmptyCharacter() {
+        for (int i = 0; i < this.ticTacToe.board.length; i++)
+            for (int j = 0; j < this.ticTacToe.board[i].length; j++)
+                assertEquals(this.ticTacToe.EMPTY, this.ticTacToe.board[i][j]);
+    }
+
+    @Test
+    void shouldPrintEmptyTable() {
+        assertEquals(
+                """
+                        +---+---+---+
+                        |   |   |   |
+                        +---+---+---+
+                        |   |   |   |
+                        +---+---+---+
+                        |   |   |   |
+                        +---+---+---+""", this.ticTacToe.printTable());
+    }
+
 }
